61d5f6cd7257d4eb90b35df9336c82f1037b218c
Anazodo Vanessa <anazodovanessa19@gmail.com>
2023-04-18 12:23:31 +0100

    init

---
+++ script.js
@@ -0,0 +1,53 @@
+function showcontent(){
+    var paragraph_One = document.getElementById('para_one');
+    if(paragraph_One.style.display == "none"){
+        paragraph_One.style.display = 'block';
+    }
+    else{
+        paragraph_One.style.display = 'none'
+    }
+}
+
+function showcontenttwo(){
+    var paragraph_two = document.getElementById('para_two');
+    if(paragraph_two.style.display == "none"){
+        paragraph_two.style.display = 'block';
+    }
+    else{
+        paragraph_two.style.display = 'none'
+    }
+}
+
+function showcontentthree(){
+    var paragraph_three = document.getElementById('para_three');
+    if(paragraph_three.style.display == "none"){
+        paragraph_three.style.display = 'block';
+    }
+    else{
+        paragraph_three.style.display = 'none'
+    }
+}
+
+function showcontentfour(){
+    var paragraph_four = document.getElementById('para_four');
+    if(paragraph_four.style.display == "none"){
+        paragraph_four.style.display = 'block';
+    }
+    else{
+        paragraph_four.style.display = 'none'
+    }
+}
+
+
+function showcontentfive(){
+    var paragraph_five = document.getElementById('para_five');
+    if(paragraph_five.style.display == "none"){
+        paragraph_five.style.display = 'block';
+    }
+    else{
+        paragraph_five.style.display = 'none'
+    }
+}
+
+
+
